24a2d0c377e7737693c0259d84be63d726f5c26f
Mike Cronin <mostlyfocusedmike@gmail.com>
2020-02-13 17:14:49 -0500

    get it sending only the message to the other user

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
+// https://blog.bitlabstudio.com/a-simple-chat-app-with-react-node-and-websocket-35d3c9835807
+
 const express = require('express')
 const app = express()
 
-let value = 'initial value';
+let logs = 'Start chatting!';
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/ws.html');
 })
 
-app.get('/test', (req, res) => {
-    res.send({ value })
-});
-
 app.listen(8765, () => {
     console.log('Example app listening on port 8765!')
 });
 
-const WebSocketServer = require('ws').Server;
-const wss = new WebSocketServer({port: 40510});
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 40510});
+
 wss.on('connection', (ws) => {
-    ws.on('message', (message) => {
-        console.log(`received: ${message}`);
-    })
-    setInterval(
-        () => ws.send(`${new Date()}`),
-        10000
-    )
+    ws.on('message', (data) => {
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                console.log('hinge');
+                client.send(data);
+            }
+        });
+    });
 });
