ff956868c0aba652397b74c8916711cb20c171f7
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2020-02-13 23:35:22 -0500

    rename html file and remove extra connected message

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // https://blog.bitlabstudio.com/a-simple-chat-app-with-react-node-and-websocket-35d3c9835807
 
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/ws.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.listen(8765, () => {
@@ -16,24 +17,18 @@ const WebSocket = require('ws');
 const socketServer = new WebSocket.Server({ port: 40510});
 let logs = ['Start Chatting!'];
 
-const isConnecting = (data) => data === 'connected';
-
 // on connection the websocket connection client is passed in
 // you can keep track of clients so send certain messages to certain clients
 socketServer.on('connection', (ws) => {
+    console.log('connection!');
+    ws.send(JSON.stringify(logs));
     ws.on('message', (data) => {
-        if (!isConnecting(data)) logs.push(data);
-
+        logs.push(data);
         socketServer.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
-                if (isConnecting(data) && ws === client) {
-                    client.send(JSON.stringify(logs));
-                } else if (!isConnecting(data)) {
-                    const messages = JSON.stringify([data]);
-                    client.send(messages);
-                }
+                const messages = JSON.stringify([data]);
+                client.send(messages);
             }
-
         });
     });
 });
