f97615bc88ef2ee997ba459150a0c7f55b99fd3c
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2020-02-18 23:10:32 -0500

    tweaks for better readability

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // https://blog.bitlabstudio.com/a-simple-chat-app-with-react-node-and-websocket-35d3c9835807
-
+const WebSocket = require('ws');
 const express = require('express');
 const path = require('path');
 const app = express()
@@ -8,22 +8,23 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.listen(8765, () => {
-    console.log('Example app listening on port 8765!')
+const port = 8765
+app.listen(port, () => {
+    console.log(`listening http://localhost:${port}`)
 });
 
-const WebSocket = require('ws');
+const socketServer = new WebSocket.Server({port: 3030});
 
-const socketServer = new WebSocket.Server({ port: 3030});
-let logs = ['Start Chatting!'];
+const messages = ['Start Chatting!'];
 
 // on connection the websocket connection client is passed in
 // you can keep track of clients so send certain messages to certain clients
-socketServer.on('connection', (ws) => {
-    console.log('connection!');
-    ws.send(JSON.stringify(logs));
-    ws.on('message', (data) => {
-        logs.push(data);
+socketServer.on('connection', (socketClient) => {
+    console.log('connected');
+    console.log('Number of clients: ', socketServer.clients.size);
+    socketClient.send(JSON.stringify(messages));
+    socketClient.on('message', (data) => {
+        messages.push(data);
         socketServer.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
                 const messages = JSON.stringify([data]);
@@ -31,4 +32,10 @@ socketServer.on('connection', (ws) => {
             }
         });
     });
+
+    socketClient.on('close', (socketClient) => {
+        console.log('closed');
+        console.log('Number of clients: ', socketServer.clients.size);
+    });
 });
+
